409e872e3b740dcbd4f8df9343cba4811797748e
LieTH <lietha.arrah12@gmail.com>
2026-08-21 19:39:15 +0300

    Add: climbing-stairs - submission-10

---
+++ Data Structures & Algorithms/climbing-stairs/submission-10.py
@@ -0,0 +1,10 @@
+class Solution:
+    def climbStairs(self, n: int, memo={}) -> int:
+
+        if n == 0 or n == 1:
+            return 1
+        if n in memo:
+            return memo[n]
+        
+        memo[n] = self.climbStairs(n-1, memo) + self.climbStairs(n-2, memo)
+        return memo[n]
